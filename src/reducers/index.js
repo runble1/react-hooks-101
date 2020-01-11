0f20c5ba8b36227b15690d2c888081f3ccadcf9e
0ky3r43m78m070b@gmail.com <0ky3r43m78m090b@gmail.com>
2020-01-11 17:35:21 +0900

    implement event creation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,39 +1,3 @@
-action = {
-  type: 'CREATE_EVENT',
-  title: '2020東京オリンピックのお知らせ',
-  body: '2020年に東京でオリンピックを開催します！つきましては、、、、、'
-}
-# before
-state = []
-# after
-state = [
-  {
-    id: 1,
-    title: 'タイトル1',
-    body: 'ボディ1',
-  }
-]
-
-# before
-state = [
-  { id: 1, title: 'タイトル1', body: 'ボディ'},
-  { id: 2, title: 'タイトル2', body: 'ボディ'},
-  { id: 3, title: 'タイトル3', body: 'ボディ'},
-}
-
-# after
-state = [
-  { id: 1, title: 'タイトル1', body: 'ボディ'},
-  { id: 2, title: 'タイトル2', body: 'ボディ'},
-  { id: 3, title: 'タイトル3', body: 'ボディ'},
-  { 
-    id: 4,
-    title: '2020東京オリンピックのお知らせ',
-    body: '2020年に東京でオリンピックを開催します！つきましては、、、、'
-  }
-]
-
-
 const events = (state = [], action) => {
   switch(action.type) {
     case'CREATE_EVENT':
@@ -47,7 +11,7 @@ const events = (state = [], action) => {
       return []
     default:
       return state
-    
+  }
 }
 
 export default events
